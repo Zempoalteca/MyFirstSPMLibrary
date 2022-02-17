76b33c5d5bc548decbec8d0df0ca0d6309629e86
Gabriel Zempoalteca <gzempoalteca@acculynx.com>
2022-02-17 11:18:47 -0600

    Added basic example function

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,6 +1,12 @@
 public struct MyLibrary {
+
     public private(set) var text = "Hello, World!"
 
     public init() {
     }
+
+    public func helloToYou() -> String {
+        return "Hello to You!"
+    }
+
 }
